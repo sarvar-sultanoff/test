0daac6312cdc15a4b112499856dc1830f3906a5c
iAmSarvar <iamsarvarsultanov@gmail.com>
2025-06-16 14:30:26 +0500

    nice little feature

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+
+if(true === true){
+    console.log("Condition is true");
+}else{
+    for(let i = 0; i < 5; i++) {
+        console.log("Loop iteration: " + i);
+    }
+    //add console log 
+    console.log("Condition is false, loop completed");
+}
